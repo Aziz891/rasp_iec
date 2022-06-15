768a127409442131c70b2394c3a7c897c50c78fa
aziz891 <aziz.aboudi89@gmail.com>
2022-06-15 15:42:29 +0100

    vacation

--- server.py
+++ server.py
@@ -1,13 +1,17 @@
+from unittest import result
 from flask import Flask, jsonify
 from flask.globals import request
 from flask_cors import CORS, cross_origin
-import subprocess
+import subprocess, re
 import argparse
 from scapy.all import *
+from psutil import net_if_addrs
+import io
 
 
 
 def arp_scan(ip):
+	
     request = Ether(dst="ff:ff:ff:ff:ff:ff") / ARP(pdst=ip)
 
     ans, unans = srp(request, timeout=5, retry=1, iface="eth0")
@@ -21,16 +25,83 @@ app = Flask(__name__)
 cors = CORS(app)
 app.config['CORS_HEADERS'] = 'Content-Type'
 
+@app.route('/6')
+@cross_origin()
+def get_comms():
+
+    ip = request.args.get('ip')
+    cmd = ["./file-tool -h {}  subdir COMTRADE".format(ip)]
+    p = subprocess.Popen(cmd, stdout = subprocess.PIPE,
+                            stderr=subprocess.PIPE,
+                            stdin=subprocess.PIPE, shell=True)
+    out,err = p.communicate()
+    y =  out.decode('ascii').split("\n")
+    list_comms = [i.split()[0] for i in y if re.match('^\S*CFG', i)]
+    result = []
+    for i in list_comms: 
+        cmd = ["./file-tool -h {} get '{}'".format(ip,i)]
+        p = subprocess.Popen(cmd, stdout = subprocess.PIPE,
+                                stderr=subprocess.PIPE,
+                                stdin=subprocess.PIPE, shell=True)
+        out,err = p.communicate()
+        result.append({"name": i,"date" : out.decode('ascii').split('\r\n')[-4]})
+
+    return jsonify(result)
+
+@app.route('/7')
+@cross_origin()
+def download_comms():
+
+    f = io.BytesIO()
+    ip = request.args.get('ip')
+    name = request.args.get('name')
+    cmd = ["./file-tool -h {}  get 'COMTRADE\{}'".format(ip, name)]
+    p = subprocess.Popen(cmd, stdout = subprocess.PIPE,
+                            stderr=subprocess.PIPE,
+                            stdin=subprocess.PIPE, shell=True)
+    out,err = p.communicate()
+    f.write(out)
+    result = f.getvalue()
+    f.close()
+
+
+
+    
+   
+
+    return (result)
+    
+
+
+    return result
+def get_name(ip):
+    cmd = ["./client_example3", ip]
+    p = subprocess.Popen(cmd, stdout = subprocess.PIPE,
+                            stderr=subprocess.PIPE,
+                            stdin=subprocess.PIPE)
+    out,err = p.communicate()
+    out = out.decode('utf-8').strip()
+    print(out[:2])
+    if out[:2] == 'LD':
+        return out[2:]
+    else:
+        return "Unknown"
 @app.route('/5')
 @cross_origin()
 def arp_scan():
-    request = Ether(dst="ff:ff:ff:ff:ff:ff") / ARP(pdst="192.168.2.1/24")
+    ip="172.16.1.1"
+
+    if net_if_addrs().get("eth0"):
+    	ip = net_if_addrs().get("eth0")[0].address
+    
+
+    request = Ether(dst="ff:ff:ff:ff:ff:ff") / ARP(pdst=f"{ip}/22")
 
     ans, unans = srp(request, timeout=5, retry=1, iface="eth0")
     result = []
 
     for sent, received in ans:
-        result.append({'IP': received.psrc, 'MAC': received.hwsrc})
+        result.append({'IP': received.psrc, 'MAC': received.hwsrc, 'NAME': get_name(received.psrc)})
 
     result = jsonify(result)
     return result   
@@ -75,3 +146,4 @@ def hello_world4():
                             stdin=subprocess.PIPE)
     out,err = p.communicate()
     return out
+
